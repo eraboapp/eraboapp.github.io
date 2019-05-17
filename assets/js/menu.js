94ddf17ffdb6f3c5f8c1eff707053ea7d887db34
Ivan Alburquerque <ivan.alburquerque@publicis.com.do>
2019-05-17 17:30:44 -0400

    Disabled scroll movement on safari ios

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,7 +4,34 @@ let body = document.querySelector('body');
 
 burgerMenu.addEventListener('click', () => {
 
+    if( burgerMenu.classList.contains('menu-on') ) {
+        document.body.removeEventListener('touchmove', preventDefault, { passive: false });
+    } else {
+        document.body.addEventListener('touchmove', preventDefault, { passive: false })
+    }
+
     burgerMenu.classList.toggle('menu-on');
     nav.classList.toggle('--menu-open');
     body.classList.toggle('no-scroll');    
 })
+
+
+nav.querySelectorAll('a')
+.forEach(link => {
+
+    link.addEventListener('click', (e) => {
+
+        if( burgerMenu.classList.contains('menu-on') ) {
+            document.body.removeEventListener('touchmove', preventDefault, { passive: false });
+
+            burgerMenu.classList.toggle('menu-on');
+            nav.classList.toggle('--menu-open');
+            body.classList.toggle('no-scroll');
+        }
+    });
+});
+
+
+function preventDefault(e) {
+    e.preventDefault();
+}
